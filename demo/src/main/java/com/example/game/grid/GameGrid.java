9583ea19285751d4d1789084a6274537dc6d14a9
Ludwig <00ludwighahn@gmail.com>
2024-08-30 15:24:34 +0200

    Logic for cells with value 5

--- demo/src/main/java/com/example/game/grid/GameGrid.java
+++ demo/src/main/java/com/example/game/grid/GameGrid.java
@@ -1,4 +1,5 @@
 package com.example.game.grid;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -7,15 +8,18 @@ import com.example.game.model.Cell;
 import com.example.game.model.Color;
 import com.example.game.model.Score;
 
-
 public class GameGrid {
     private Cell[][] grid;
     private Map<Integer, Color> numberToColorMap;
     private Score score;
+    private int gridRow;
+    private int gridCol;
 
     public GameGrid(int row, int col) {
         grid = new Cell[row][col];
         score = new Score();
+        this.gridRow = row;
+        this.gridCol = col;
         initializeNumberToColorMap();
         createGridArray();
     }
@@ -31,11 +35,11 @@ public class GameGrid {
 
     private void createGridArray() {
         Random random = new Random();
-       // System.out.print("\nCreating 2d array...");
+        // System.out.print("\nCreating 2d array...");
         for (int i = 0; i < this.grid.length; i++) {
             for (int j = 0; j < this.grid[i].length; j++) {
-                 int randomNumber = random.nextInt(5) + 1;
-                //int randomNumber = 1;
+                int randomNumber = random.nextInt(4) + 1;
+                // int randomNumber = 2;
                 Color associatedColor = numberToColorMap.get(randomNumber);
                 this.grid[i][j] = new Cell(randomNumber, associatedColor);
             }
@@ -43,38 +47,104 @@ public class GameGrid {
         System.out.print("done\n");
     }
 
-    private void removeCell(int row, int col) {
-        // If there are cells above the selectedCell, move them one step down.
-        int topCell = row;
+    private void addCellAtTopRow(int row1, int col1, int row2, int col2) {
+        // Fill the top with new cells and push them down
+        Random random = new Random();
+        int counter = 0;
+        for (int i = 0; i < gridCol; i++) {
+            if (i == col1) { // cells is horizontel
+                moveCellsDownCol2(col1);
 
+            } else if (i == col2 && !(col1 == col2))
+                moveCellsDownCol(row2, col2);
+
+            if (grid[0][i] == null) {
+                while ((counter != (this.gridRow - 1)) && grid[counter + 1][i] == null) {
+                    counter++;
+                }
+                // counter++;
+                int randomNumber = random.nextInt(3) + 1;
+                Color associatedColor = numberToColorMap.get(randomNumber);
+                grid[counter][i] = new Cell(randomNumber, associatedColor);
+                counter = 0;
+            }
+        }
+    }
+
+    private void moveCellsDownCol2(int col) {
+        int numRows = grid.length;
+
+        // Start from the bottom and move upwards
+        for (int row = numRows - 1; row > 0; row--) {
+            if (grid[row][col] == null) {
+                // Find the topmost non-null cell above the current row
+                for (int topRow = row - 1; topRow >= 0; topRow--) {
+                    if (grid[topRow][col] != null) {
+                        grid[row][col] = grid[topRow][col]; // Move the cell down
+                        grid[topRow][col] = null; // Set the original position to null
+                        System.out.println("Moved Cell: (" + topRow + ", " + col + ") to (" + row + ", " + col + ")");
+                        break; // Move to the next row down
+                    }
+                }
+            }
+        }
+    }
+
+    private void moveCellsDownCol(int row, int col) {
+        int topCell = row;
         for (int i = row; i > 0; i--) {
             if (grid[i - 1][col] == null) {
-                grid[i][col] = null;
+                grid[topCell][col] = null;
                 return;
             }
             grid[i][col] = grid[i - 1][col];
-            System.out.println("Swapped Cell: (" + i + ", " + col + ") with " + "(" + (i - 1) + ", " + col + ")");
+            System.out.println("Moved Cell: (" + (i - 1) + ", " + col + ") to " + "(" + i + ", " + col + ")");
             topCell--;
+
         }
-
         grid[topCell][col] = null;
-        // System.out.println("Removed Cell: (" + topCell + ", " + col + ")");
+    }
 
+    // TODO just create a function that sets a cell to null.
+
+    public boolean mergeCells(int row1, int col1, int row2, int col2) {
+        // row1 and col1 is the cordniates of the first selected cell
+        if (isValidSelection(row1, col1, row2, col2)) {
+            Cell cell1 = grid[row1][col1];
+            Cell cell2 = grid[row2][col2];
+
+            if (cell1.getNumber() == 5) {
+                score.incrementScore(cell2.getNumber() - 1);
+                grid[row1][col1] = null;
+                grid[row2][col2] = null;
+                addCellAtTopRow(row1, col1, row2, col2);
+            } else {
+                cell2.setNumber(cell2.getNumber() + 1);
+                cell2.setColor(numberToColorMap.get(cell2.getNumber()));
+                System.out.println("Upgraded Cell: (" + row2 + ", " + col2 + ") from: " + (cell2.getNumber() - 1)
+                        + " to: " + cell2.getNumber());
+                score.incrementScore(cell2.getNumber() - 1);
+                this.grid[row1][col1] = null;
+                moveCellsDownCol(row1, col1);
+
+            }
+
+            return true;
+
+        }
+        return false;
     }
 
     private boolean isValidSelection(int row1, int col1, int row2, int col2) {
         // Ensure the selections are not null
-        if (this.grid[row1][col1] == null || this.grid[row2][col2] == null){
+        if (this.grid[row1][col1] == null || this.grid[row2][col2] == null) {
             System.out.println("Selected a empty sqaure!");
             return false;
         }
-
-        //Ensure the selections is not the same
-        if(row1 == row2 && col1 == col2){
-            System.out.println("Selected the same square!");
+        if (grid[row1][col1].getNumber() != grid[row2][col2].getNumber()) {
+            System.out.println("Numbers are not equal!");
             return false;
         }
-
         // Ensure the selections are within bounds
         if (row1 >= 0 && row1 < grid.length && col1 >= 0 && col1 < grid[0].length &&
                 row2 >= 0 && row2 < grid.length && col2 >= 0 && col2 < grid[0].length) {
@@ -82,24 +152,6 @@ public class GameGrid {
             if ((Math.abs(row1 - row2) == 1 && col1 == col2) || // vertical
                     (Math.abs(col1 - col2) == 1 && row1 == row2) || // horizontal
                     (Math.abs(row1 - row2) == 1 && Math.abs(col1 - col2) == 1)) { // diagonal
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean mergeCells(int row1, int col1, int row2, int col2) {
-        // row1 and col1 is the cordniates of the first selected cell
-        if (isValidSelection(row1, col1, row2, col2)) {
-            Cell cell1 = grid[row1][col1];
-            Cell cell2 = grid[row2][col2];
-            if (cell1.getNumber() == cell2.getNumber()) {
-                cell2.setNumber(cell2.getNumber() + 1);
-                cell2.setColor(numberToColorMap.get(cell2.getNumber()));
-                System.out.println("Upgraded Cell: (" + row2 + ", " + col2 + ") from: " + (cell2.getNumber()-1) + " to: " + cell2.getNumber());
-                removeCell(row1, col1);
-                score.incrementScore(cell2.getNumber()-1);
-
                 return true;
             }
         }
